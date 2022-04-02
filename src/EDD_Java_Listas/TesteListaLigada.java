271a5fd6db40635664e4278d454c7939deb13f52
Mariana <marianalimajanuario@gmail.com>
2022-04-02 20:12:09 -0300

    Second commit

---
+++ src/EDD_Java_Listas/TesteListaLigada.java
@@ -0,0 +1,15 @@
+package EDD_Java_Listas;
+
+public class TesteListaLigada {
+    public static void main(String[] args) {
+        ListaLigada lista = new ListaLigada();
+
+        System.out.println(lista);
+        lista.adicionaNoComeco("mauricio");
+        System.out.println(lista);
+        lista.adicionaNoComeco("paulo");
+        System.out.println(lista);
+        lista.adicionaNoComeco("guilherme");
+        System.out.println(lista);
+    }
+}
